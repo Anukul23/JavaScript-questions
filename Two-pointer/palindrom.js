27bdfdb88995922afcaf6f3cd5c8f9a74a58924e
Anukul23 <141473676+Anukul23@users.noreply.github.com>
2025-10-15 18:33:46 +0530

    palidrom-problem solve

---
+++ Two-pointer/palindrom.js
@@ -0,0 +1,17 @@
+
+
+const palindrom = (str)=>{
+
+    let l =0 ;
+    let r = str.length-1;
+
+    while(l<r){
+        if (str[l] != str[r]){
+            return false;
+        }
+        l++;
+        r--
+    }
+    return true;
+}
+console.log(palindrom("crio"));
