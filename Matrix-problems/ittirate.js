5adb2bcb35e0c0a4f38a453a5d7548bb292a1d28
Anukul23 <141473676+Anukul23@users.noreply.github.com>
2025-10-17 11:36:21 +0530

    Matrix itirate

---
+++ Matrix-problems/ittirate.js
@@ -0,0 +1,16 @@
+
+
+let matrix = [
+    [1,2,3],
+    [3,4,5],
+    [6,7,8]
+]
+
+const itirate =(matrix )=>{
+    for(let i =0 ;i <matrix.length ; i++){
+        for(let j=0 ;j<matrix[i].length;j++){
+            console.log(matrix[i][j])
+        }
+    } 
+}
+itirate(matrix);
